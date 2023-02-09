85d77d92da14c9324ff18b546c1f5c0fdf1e448e
Zakhar Petukhov <zakharpetukhov@protonmail.com>
2023-02-09 13:58:24 +0800

    move spam rules into separate module

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/tonkeeper/scam_backoffice_rules
+
+go 1.19
+
+require (
+	github.com/labstack/gommon v0.4.0
+	golang.org/x/text v0.7.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+)
---
+++ rules.go
@@ -0,0 +1,87 @@
+package scam_backoffice_rules
+
+import (
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"github.com/labstack/gommon/log"
+	"gopkg.in/yaml.v3"
+	"regexp"
+)
+
+type TypeOfAction string
+
+const (
+	Accept  TypeOfAction = "accept"
+	Drop    TypeOfAction = "drop"
+	UnKnown TypeOfAction = "unknown"
+)
+
+type ConvertedRules struct {
+	Rules []struct {
+		Pattern string       `yaml:"pattern" json:"pattern"`
+		Action  TypeOfAction `yaml:"action" json:"action"`
+	} `yaml:"rules" json:"rules"`
+}
+
+type Rule struct {
+	Evaluate func(comment string) TypeOfAction
+}
+
+type Rules []Rule
+
+func LoadRules(bytesOfRules []byte, yamlConverted bool) Rules {
+	var rules Rules
+	var convertedRules ConvertedRules
+	var err error
+
+	if yamlConverted {
+		err = yaml.Unmarshal(bytesOfRules, &convertedRules)
+	} else {
+		err = json.Unmarshal(bytesOfRules, &convertedRules)
+	}
+	if err != nil {
+		log.Panicf("Failed to parse rules: %v", err)
+	}
+
+	for _, inputRule := range convertedRules.Rules {
+		compiledRegexp, err := regexp.Compile(inputRule.Pattern)
+		if err != nil {
+			fmt.Printf("Failed to compile regexp for pattern %s: %v", inputRule.Pattern, err)
+			continue
+		}
+
+		var rule Rule
+		action := inputRule.Action
+		rule.Evaluate = func(comment string) TypeOfAction {
+			match := compiledRegexp.MatchString(comment)
+			if !match {
+				return UnKnown
+			}
+			return action
+		}
+
+		rules = append(rules, rule)
+	}
+
+	return rules
+}
+
+func CheckAction(rules Rules, comment string) TypeOfAction {
+	var err error
+
+	comment, err = NormalizeComment(comment)
+	if err != nil {
+		return Drop
+	}
+
+	action := UnKnown
+	for _, rule := range rules {
+		action = rule.Evaluate(comment)
+		if action != UnKnown {
+			break
+		}
+	}
+
+	return action
+}
